965f5121853364d003a4e49b755066e108c6fa0e
w1nteRR <mykola796@gmail.com>
2023-09-21 17:33:32 +0300

    update favorites

--- src/pages/favorites/favorites.page.tsx
+++ src/pages/favorites/favorites.page.tsx
@@ -1,8 +1,14 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import { useTelegram } from '../../hooks/telegram/useTelegram'
+import { useBackButton } from '../../hooks/telegram/useBackButton'
 
 export const FavoritesPage: FC = () => {
+  const navigate = useNavigate()
   const { themeParams } = useTelegram()
+
+  useBackButton(() => navigate(-1))
 
   return <p style={{ color: themeParams.text_color }}>favorites page</p>
 }
