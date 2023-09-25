bea4722177253813e5dcd262c99489f8345ed224
w1nteRR <mykola796@gmail.com>
2023-09-26 02:07:03 +0300

    refactor chip

--- src/components/ui/chip/chip.ui.tsx
+++ src/components/ui/chip/chip.ui.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useMemo } from 'react'
+import { FC, ReactNode } from 'react'
 import { useTelegram } from '../../../hooks/telegram/useTelegram'
 
 interface IChipProps {
@@ -8,23 +8,14 @@ interface IChipProps {
 }
 
 export const Chip: FC<IChipProps> = ({ isActive, children, onClick }) => {
-  const { themeParams, colorScheme } = useTelegram()
-
-  const backgroundColor = useMemo(() => {
-    if (isActive && colorScheme === 'dark') return themeParams.bg_color
-    if (isActive && colorScheme === 'light') return themeParams.bg_color
-
-    return 'unset'
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isActive, colorScheme])
+  const { themeParams } = useTelegram()
 
   return (
     <div
       onClick={onClick}
       className='py-2 px-5 mr-5 rounded-full font-semibold text-sm'
       style={{
-        backgroundColor,
+        backgroundColor: isActive ? themeParams.bg_color : 'unset',
       }}
     >
       {children}
