862c4964b3270920a76097e103629cff68c57496
w1nteRR <mykola796@gmail.com>
2024-01-12 00:39:13 +0200

    add recent hook

--- src/hooks/recent/useRecentUsers.ts
+++ src/hooks/recent/useRecentUsers.ts
@@ -1,18 +1,76 @@
-import { db } from '../../db/recent-users.db'
+import { useEffect, useState } from 'react'
+import { CloudStorage, CloudStorageKeys } from '../../helpers/cloud-storage'
 import { IRecentUser } from '../../types/user/user.types'
 
+type List = Array<IRecentUser>
+
 export const useRecentUsers = () => {
-  const addUserToRecentList = async (recentUser: IRecentUser) => {
-    if (!recentUser) return
+  const [recentUsers, setRecentUsers] = useState<List>([])
 
-    const isUserExist = await db.recentUsers.get({ id: recentUser.id })
+  const updateRecentUsers = async (list: List) => {
+    const value = JSON.stringify(list)
 
-    if (isUserExist) return
+    await CloudStorage.setValue(CloudStorageKeys.Recent, value)
 
-    db.recentUsers.add(recentUser)
+    setRecentUsers(list)
   }
 
+  const addUserToRecentCloudStorage = async (user: IRecentUser) => {
+    let list = [...recentUsers]
+
+    try {
+      const isUserExist = recentUsers.some(
+        (recentUser) => recentUser.id === user.id
+      )
+
+      if (isUserExist) return
+
+      list.push(user)
+
+      await updateRecentUsers(list)
+    } catch (error) {
+      console.log('USER SAVE ERROR', error)
+    }
+  }
+
+  const removeUserFromRecentCloudStorage = async (id: string) => {
+    let list = [...recentUsers]
+
+    try {
+      list = list.filter((user) => user.id !== id)
+
+      await updateRecentUsers(list)
+    } catch (error) {
+      console.log('USER REMOVE ERROR', error)
+    }
+  }
+
+  const removeAllUsersFromRecentCloudStorage = async () => {
+    try {
+      await updateRecentUsers([])
+    } catch (error) {
+      console.log('RECENT REMOVE ALL ERROR', error)
+    }
+  }
+
+  const loadRecentUsers = async () => {
+    try {
+      const recentUsers = await CloudStorage.values(CloudStorageKeys.Recent)
+
+      setRecentUsers(recentUsers)
+    } catch (error) {
+      console.log('LOAD ERROR', error)
+    }
+  }
+
+  useEffect(() => {
+    loadRecentUsers()
+  }, [])
+
   return {
-    addUserToRecentList,
+    addUserToRecentCloudStorage,
+    removeUserFromRecentCloudStorage,
+    removeAllUsersFromRecentCloudStorage,
+    recentUsers,
   }
 }
