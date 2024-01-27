84c58cbea88f46e215c81b184a6d25abc508f404
w1nteRR <mykola796@gmail.com>
2024-01-27 02:40:37 +0200

    add notification route

--- src/pages/home/home-v2.page.tsx
+++ src/pages/home/home-v2.page.tsx
@@ -3,13 +3,14 @@ import { FC } from 'react'
 import { Header } from '../../components/header/header'
 import { RecentListV3 } from '../../components/recent-list/recent-list.v3'
 import { RecentListV4 } from '../../components/recent-list/recent-list.v4'
-import { useTranslation } from 'react-i18next'
+import { Tracking } from '../../components/tracking/tracking'
 
 export const HomeV2Page: FC = () => {
   return (
     <>
       <Header />
       <RecentListV3 />
+      <Tracking />
       <RecentListV4 />
     </>
   )
