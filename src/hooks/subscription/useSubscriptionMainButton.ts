0d0eec6ca3357b5a5494929f79681ea83cf937d6
w1nteRR <mykola796@gmail.com>
2024-02-27 00:25:01 +0200

    update useSubscriptionMainButton.ts

--- src/hooks/subscription/useSubscriptionMainButton.ts
+++ src/hooks/subscription/useSubscriptionMainButton.ts
@@ -1,15 +1,40 @@
 import { useEffect } from 'react'
 import { useTelegram } from '../telegram/useTelegram'
+import { useMutation } from 'react-query'
+import { subscriptionApi } from '../../api/subscription.api'
 
 export const useSubscriptionMainButton = () => {
-  const { MainButton, onEvent, offEvent } = useTelegram()
+  const {
+    MainButton,
+    onEvent,
+    offEvent,
+    initDataUnsafe: { user },
+    close,
+  } = useTelegram()
+  const mutation = useMutation(subscriptionApi.configureInvoice)
 
-  const handleMainButtonPress = async () => {}
+  const handleMainButtonPress = async () => {
+    if (!user?.id) return
+
+    await mutation.mutateAsync({ account_id: String(user.id) })
+
+    close()
+  }
 
   useEffect(() => {
     MainButton.setText('Checkout for 2,49 US$ / month')
     MainButton.show()
   }, [])
+
+  useEffect(() => {
+    if (mutation.isLoading) {
+      MainButton.showProgress()
+
+      return
+    }
+
+    MainButton.hideProgress()
+  }, [mutation.isLoading])
 
   useEffect(() => {
     onEvent('mainButtonClicked', handleMainButtonPress)
