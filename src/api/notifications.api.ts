4811016a62c572654b8223bbc9beb38c6ece7614
w1nteRR <mykola796@gmail.com>
2024-04-04 01:52:43 +0300

    refactor notifications hooks and api

--- src/api/notifications.api.ts
+++ src/api/notifications.api.ts
@@ -1,14 +1,16 @@
 import { mainInstance } from './axios.config'
 import {
+  INotification,
   INotificationCreatePayload,
   INotificationDeletePayload,
-  Notifications,
+  INotificationUpdatePayload,
 } from '../types/notifications/notifications.types'
 
-const url = '/notifications-queue'
+const url = 'https://ia-bot-notifications-yhawj2jxja-ez.a.run.app'
+
 export const NotificationsApi = {
   async createNotifications(payload: INotificationCreatePayload) {
-    return mainInstance.post(url, JSON.stringify(payload), {
+    return mainInstance.post(`${url}/notifications`, payload, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -16,8 +18,14 @@ export const NotificationsApi = {
   },
 
   async getNotifications(accountId: number) {
-    return mainInstance.get<Notifications>(url, {
-      params: { id: accountId },
+    return mainInstance.get<INotification>(`${url}/notifications/${accountId}`)
+  },
+
+  async updateNotification(payload: INotificationUpdatePayload) {
+    return mainInstance.patch(`${url}/notifications`, payload, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
   },
 
--- src/hooks/queries/notifications/useGetNotificationsQuery.ts
+++ src/hooks/queries/notifications/useGetNotificationsQuery.ts
@@ -14,10 +14,9 @@ export const useGetNotificationsQuery = () => {
   const { list: favorites } = useFavorites()
 
   return useQuery([key], () => NotificationsApi.getNotifications(user?.id!), {
+    retry: false,
     select: (data) => {
-      const notificationIds = data.data.flatMap(
-        (notification) => notification.ids,
-      )
+      const notificationIds = data.data.ids
 
       return favorites.filter((favorite) =>
         notificationIds.includes(Number(favorite.id)),
