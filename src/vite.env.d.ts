9601e74308e1cf957e5614cb764dec94452c84bc
w1nteRR <mykola796@gmail.com>
2024-06-04 00:36:10 +0300

    update env types

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_WEB_URL: string
   readonly VITE_BASE_URL: string
   readonly VITE_NOTIFICATIONS_SERVICE_URL: string
+  readonly VITE_WALLET_PAY_SERVICE_URL: string
+  readonly VITE_USER_VALIDATION_FUNCTION_URL: string
 }
 
 interface ImportMeta {
