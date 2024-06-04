2558eff9fc68996ea35d65681dc46bb9e8e9ceaf
w1nteRR <mykola796@gmail.com>
2024-06-05 01:22:17 +0300

    remove unused

--- src/components/ui/typography/description.ui.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode, FC } from 'react'
-
-export const Description: FC<{ children: ReactNode }> = ({ children }) => {
-  return <p className='text-zinc-500 text-sm font-medium'>{children}</p>
-}
