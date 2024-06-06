4d6b73e9c22b7a75f9270093ab8f56ebb954ef3c
w1nteRR <mykola796@gmail.com>
2024-06-07 00:38:14 +0300

    fix validate url

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -4,7 +4,7 @@ import { ITgUser } from '../types/user/tg-user.types'
 export const AuthApi = {
   async validate(initData: string) {
     const mode = import.meta.env.MODE
-    const url = `/user-validation${mode === 'development' && '-dev'}`
+    const url = `/user-validation${mode === 'development' ? '-dev' : ''}`
 
     return mainInstance.get<ITgUser>(url, {
       headers: { 'init-tg': initData, 'Content-Type': 'application/json' },
