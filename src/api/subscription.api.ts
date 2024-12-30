22fae16bf9a783c9638492e26c990bb0f2c6254f
w1nteRR <mykola796@gmail.com>
2024-12-30 02:59:44 +0200

    update subscription api

--- src/api/subscription.api.ts
+++ src/api/subscription.api.ts
@@ -1,9 +1,14 @@
 import { mainInstance } from './axios.config'
-import { IConfigureInvoicePayload, IConfigureInvoiceResponse } from '../types/api/subscription.types'
+import {
+  IConfigureInvoicePayload,
+  IConfigureInvoiceResponse,
+} from '../types/api/subscription.types'
 
-const url = 'https://europe-west1-ia-bot-api.cloudfunctions.net/subscription'
 export const subscriptionApi = {
   async configureInvoice(payload: IConfigureInvoicePayload) {
-    return mainInstance.post<IConfigureInvoiceResponse>(url, payload)
+    return mainInstance.post<IConfigureInvoiceResponse>(
+      '/subscription',
+      payload,
+    )
   },
 }
