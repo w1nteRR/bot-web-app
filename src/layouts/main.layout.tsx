44cb467be37953b361246bd4769016ca7054f1fa
w1nteRR <mykola796@gmail.com>
2025-01-02 00:57:18 +0200

    fix layout

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -4,24 +4,15 @@ import { useRoutes } from 'react-router-dom'
 import { SpinLoader } from '../components/ui/loaders/spin-loader'
 
 import { routes } from '../navigation/routes'
-import { useTelegram } from '../hooks/telegram/useTelegram'
 import { useWebAppUserContext } from '../hooks/context/useWebAppUserContext'
+import { useMessages } from '../hooks/messages/useMessages'
 
 export const MainLayout: FC = () => {
   const element = useRoutes(routes)
-  const { themeParams } = useTelegram()
   const { isLoading } = useWebAppUserContext()
+  useMessages()
 
   if (isLoading) return <SpinLoader fullscreen />
 
-  return (
-    <div
-    // style={{
-    //   minHeight: '100vh',
-    //   backgroundColor: themeParams.secondary_bg_color,
-    // }}
-    >
-      {element}
-    </div>
-  )
+  return <div className='max-w-md m-auto'>{element}</div>
 }
